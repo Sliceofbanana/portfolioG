8b038c6f166481af8d2aabd3c0dcb22cae62e625
Genesis Jr <sliceofbanana02@gmail.com>
2024-08-21 15:50:29 +0800

    121

--- script.js
+++ script.js
@@ -72,3 +72,21 @@ document.addEventListener('mousemove', function(e) {
   }, 500);
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll('.section');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('section-visible');
+        observer.unobserve(entry.target); 
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+});
