a98facb88e0a4283cfc3c7b5dd06e00f3a976c7e
Genesis Jr <sliceofbanana02@gmail.com>
2024-08-21 14:50:42 +0800

    21

--- script.js
+++ script.js
@@ -6,17 +6,7 @@ function toggleMenu() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('section');
   const navLinks = document.querySelectorAll('.nav-links a');
-
-  function updateActiveLink() {
-    let index = sections.length;
-
-    while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
-
-    navLinks.forEach((link) => link.classList.remove('active'));
-    navLinks[index].classList.add('active');
-  }
 
   navLinks.forEach(link => {
     link.addEventListener('click', function(event) {
@@ -26,15 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
       const targetElement = document.getElementById(targetId);
 
       if (targetElement) {
+        history.pushState(null, null, `/${targetId}`);
         targetElement.scrollIntoView({
           behavior: 'smooth',
           block: 'start'
         });
+        navLinks.forEach(link => link.classList.remove('active'));
+        this.classList.add('active');
       }
     });
   });
 
-  window.addEventListener('scroll', updateActiveLink);
-  updateActiveLink();
-});
+  window.addEventListener('popstate', () => {
+    const path = window.location.pathname.substring(1);
+    const targetElement = document.getElementById(path);
 
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
